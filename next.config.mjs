f53cf4d152a3e16053a4b2858c0ff540474f735c
Faizalkusumadan <fazadanki@gmail.com>
2026-09-26 09:17:37 +0700

    testing-deploy-02

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     qualities: [25, 50, 65, 70, 75, 80, 100],
   },
